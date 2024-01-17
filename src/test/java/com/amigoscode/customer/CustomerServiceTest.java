c18a8cb0fc5c870806a70cf76fa8486f813deae5
mac_book_pro <azimovm.j@gamil.com>
2024-01-17 04:36:29 -0500

    CustomerJPADataAccessService CustomerServiceTest Prepared Before Each Test and adding Mock Annotation

--- src/test/java/com/amigoscode/customer/CustomerServiceTest.java
+++ src/test/java/com/amigoscode/customer/CustomerServiceTest.java
@@ -2,11 +2,14 @@ package com.amigoscode.customer;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@ExtendWith(MockitoExtension.class)
 class CustomerServiceTest {
 
     @Mock
