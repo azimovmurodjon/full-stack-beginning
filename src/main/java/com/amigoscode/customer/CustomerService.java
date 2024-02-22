c0613ea4f933c5ebe40fa4053c5ecf04cf9036e4
azimovmurodjon <muradjanazimov@gmail.com>
2024-02-22 00:33:54 -0500

    No Change

--- src/main/java/com/amigoscode/customer/CustomerService.java
+++ src/main/java/com/amigoscode/customer/CustomerService.java
@@ -35,7 +35,6 @@ public class CustomerService {
                     "Email already taken"
             );
         }
-
         // add
         Customer customer = new Customer(
                 customerRegistrationRequest.name(),
