b620d7dea48277628c6e64bb85c3bfa55dc65851
azimovmurodjon <muradjanazimov@gmail.com>
2024-02-25 13:01:42 -0500

    No Change

--- src/main/java/com/amigoscode/customer/CustomerService.java
+++ src/main/java/com/amigoscode/customer/CustomerService.java
@@ -52,6 +52,7 @@ public class CustomerService {
                     "Customer with id [%s] not found".formatted(customerId)
             );
         }
+
         customerDao.deleteCustomerById(customerId);
     }
     public void updateCustomer(Integer customerId,
