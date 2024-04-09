55889694d69c2b838f3424dade69030a5887f800
mac_book_pro <azimovm.j@gamil.com>
2024-04-08 21:15:07 -0400

    No Change

--- src/test/java/com/amigoscode/customer/CustomerServiceTest.java
+++ src/test/java/com/amigoscode/customer/CustomerServiceTest.java
@@ -79,6 +79,7 @@ class CustomerServiceTest {
         int id = 10;
         String email = "alex@gmail.com";
 
+
         when(customerDao.existsPersonWithEmail(email)).thenReturn(false);
 
         CustomerRegistrationRequest request = new CustomerRegistrationRequest(
